15266a49d87f16c7056c8ed7159b6576d683bfb8
Dorian Dayvid Gomes Feitosa <dorian.dayvid@aluno.ufca.edu.br>
2025-11-19 00:19:30 +0000

    Adicionei encapsulamentos (eu acho)

---
+++ Filtro.py
@@ -0,0 +1,10 @@
+class Filtro:
+     """
+    Classe para armazenar os filtros a serem criados e suas características
+    """
+    def __init__(self,livro):
+        self.livro = livro
+
+    
+    def filtrar(self):
+      pass
