c4bdc246d47bfae8f91502e59946007812e9028a
Dorian Dayvid Gomes Feitosa <dorian.dayvid@aluno.ufca.edu.br>
2025-11-25 23:50:33 +0000

    Alterado o main

---
+++ main.py
@@ -0,0 +1,17 @@
+import Anotacao
+
+
+print("Bom dia")
+
+print("bem vindo ao menu da Biblioteca Digital mágica: selecione uma das funções para começar:" \
+"1 - Adicionar Livro" \
+"2 - Adicionar Revista" \
+"3 - Fazer anotação" \
+"4 - Dar nota a um livro" \
+"5 - Alterar Status de leitura" \
+"6 - Buscar Filtros" \
+"7 - Visualizar relatórios")
+opcao = input()
+
+if opcao = 1:
+    print("")
